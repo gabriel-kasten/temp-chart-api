e56cda38afb30108761989a546c333e5c94bb960
gabriel-kasten <gabrieldegkasten@gmail.com>
2024-02-20 23:45:26 -0300

    api completed using gin

---
+++ api/database/database.go
@@ -0,0 +1,50 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Result struct {
+	ID           int     `json:"id"`
+	DateMeasured string  `json:"date_measured"`
+	Temperature  float64 `json:"temperature"`
+}
+
+func DatabaseCon() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./database/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, err
+}
+
+func QueryTemperatureData(db *sql.DB) ([]Result, error) {
+	var results []Result
+
+	rows, err := db.Query("SELECT id, date_measured, temperature FROM TEMPERATURE_DATA")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var r Result
+		err = rows.Scan(&r.ID, &r.DateMeasured, &r.Temperature)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, r)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return results, nil
+
+}
---
+++ api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/temp-chart-go/database"
+)
+
+func main() {
+	db, err := database.DatabaseCon()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := gin.Default()
+	app.GET("/temperature-data", func(ctx *gin.Context) {
+
+		results, err := database.QueryTemperatureData(db)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, results)
+
+	})
+	app.Run(":6969")
+}
